9ea4af66fe279af164dd5e3d97c1fff0b43968a5
jacksonnqz <jackson@spiekerpoint.com>
2026-08-20 19:43:25 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import * as aws from "@pulumi/aws";
+
+const bucket = new aws.s3.Bucket("rocketseat-iac", {
+  bucket: "rocketseat-iac",
+  tags: {
+    IAC: "true",
+  },
+});
+
+const ecr = new aws.ecr.Repository("rocketseat-iac", {
+  name: "rocketseat-iac",
+  imageTagMutability: "IMMUTABLE",
+  tags: {
+    IAC: "true",
+  },
+});
+
+export const bucketName = bucket.id;
+export const bucketRegion = bucket.region;
+export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name
+export const ecrRepositoryUrl = ecr.repositoryUrl
